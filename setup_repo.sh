e100d4dd56723daa002111beacb2a8951294d26a
Reina Smith <I-love-linux-12-31@users.noreply.github.com>
2026-07-03 08:14:19 +0300

    Add .deb repository support

--- setup_repo.sh
+++ setup_repo.sh
@@ -19,11 +19,29 @@ setup_RHEL ()
   fi;
 }
 
+setup_DEB ()
+{
+  curl -fsSL https://repo.rnbr-dev.com/rpm/repo-key.asc \
+    | gpg --dearmor -o /etc/apt/keyrings/repo.rnbr_dev.gpg
+  
+  url="https://raw.githubusercontent.com/R-BR-dev/repository/refs/heads/main/deb.rnbr-dev.repo.sources"
+  repo_file="/etc/apt/sources.list.d/deb.rnbr-dev.repo.sources"
+  wget_installed=0
+  which wget 2>/dev/null 1>/dev/null && wget_installed=1
+  if [ $wget_installed == 1 ]; then
+    wget $url -o $repo_file
+  else
+    curl -fsSL $url -o $repo_file
+  fi;
+}
+
 
 if is_user_root;
 then (
   if [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/centos-release || -e /etc/fedora-release || $FORCE_RHEL ]]; then
     setup_RHEL;
+  elif [[ -e /etc/debian_version || $FORCE_DEB ]]; then
+    setup_DEB;
   else
     echo "Not supported system."
   fi;
